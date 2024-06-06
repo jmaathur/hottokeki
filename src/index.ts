5c5692f7c1ae7d7f02c86d3c2bae2cd91a0e91e7
Jeevan Maathur <jmaathur@gmail.com>
2024-06-06 14:37:06 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+program.name("hottokeki").description("Hottokeki CLI").version("0.0.1");
+
+program
+  .command("helloworld [fields...]")
+  .description("prints hello world")
+  .action(() => {
+    console.log("Hello world");
+  });
+
+program.parse(process.argv);
